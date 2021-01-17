2c8cf80295542c9cbd17c78a33e3c95e5ef26ea6
Neomar706 <neomarp706@gmail.com>
2021-01-17 13:48:15 -0400

    zeet deploy

---
+++ src/models/Note.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const NoteSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    user: {
+      type: String,
+      required: true
+    }
+},{
+    timestamps: true,
+    versionKey: false
+});
+
+module.exports = model("Note", NoteSchema);
